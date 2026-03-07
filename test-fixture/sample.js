5e40bad2a600b1e52612ddde0bd99ee69baa976d
Nicholas Ashkar <Nick@cirvgreen.com>
2026-03-07 12:44:37 +0400

    add test fixture

---
+++ test-fixture/sample.js
@@ -0,0 +1,18 @@
+// Sample file for testing todo-archaeologist
+
+function doSomething() {
+  // TODO: refactor this function
+  const x = 1 + 1;
+  
+  // FIXME: handle null case here
+  return x;
+}
+
+function legacyAuth() {
+  // HACK: bypass auth check for now
+  // WORKAROUND: temp fix until new API ships
+  return true;
+}
+
+// XXX: this entire module needs a rewrite
+// TEMP: remove before v2.0
